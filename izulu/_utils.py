54d1450510eb87cfc6637e0c29a4b4c47eb35f8f
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-11-18 04:09:12 +0000

    Rework

--- izulu/_utils.py
+++ izulu/_utils.py
@@ -1,9 +1,11 @@
+import dataclasses
 import string
+import types
 import typing as t
 
 
-def join(items: t.Iterable[str]) -> str:
-    return ", ".join(items)
+def join(items: t.Iterable[str], symbol: str = ",") -> str:
+    return f"{symbol} ".join(items)
 
 
 def join_kwargs(**kwargs: t.Any) -> str:
@@ -17,7 +19,18 @@ def extract_fields(template: str) -> t.Generator[str, None, None]:
             yield fn
 
 
-def extract_hints(klass: type) -> t.Generator[str, None, None]:
-    for field in t.get_type_hints(klass):
-        if field is not t.ClassVar:
-            yield field
+def filter_hints(cls: t.Type) -> t.Generator[tuple[str, t.Type], None, None]:
+    for k, v in t.get_type_hints(cls).items():
+        if t.get_origin(v) is not t.ClassVar:
+            yield k, v
+        # if k.startswith("_Error__"):
+        #     continue
+
+
+# TODO(d.burmistrov): dataclass options
+@dataclasses.dataclass
+class Store:
+    fields: frozenset[str]
+    hints: types.MappingProxyType[str, t.Type]
+    registered: frozenset[str]
+    defaults: frozenset[str]
--- izulu/root.py
+++ izulu/root.py
@@ -1,6 +1,7 @@
 import copy
 import enum
 import functools
+import types
 import typing as t
 
 from izulu import _utils
@@ -9,67 +10,95 @@ from izulu import _utils
 class Features(enum.Flag):
     FORBID_MISSING_FIELDS = enum.auto()
     FORBID_UNDECLARED_FIELDS = enum.auto()
+    FORBID_WRONG_TYPES = enum.auto()
 
     DEFAULT = FORBID_MISSING_FIELDS | FORBID_UNDECLARED_FIELDS
+    ALL = DEFAULT | FORBID_WRONG_TYPES
+    NONE = 0
 
 
 class Error(Exception):
-    __template__: t.ClassVar[str] = "Unknown exception"
+    __template__: t.ClassVar[str] = "Unspecified error"
     __features__: t.ClassVar[Features] = Features.DEFAULT
 
-    __fields: t.ClassVar[frozenset[str]]
-    __hints: t.ClassVar[frozenset[str]]
-    __registered: t.ClassVar[frozenset[str]]
-    __defaults: t.ClassVar[frozenset[str]]
+    __cls_store: t.ClassVar[_utils.Store]
 
     def __init_subclass__(cls, **kwargs: t.Any) -> None:
         super().__init_subclass__(**kwargs)
-        cls.__fields = frozenset(_utils.extract_fields(cls.__template__))
-        cls.__hints = frozenset(_utils.extract_hints(cls))
-        cls.__registered = cls.__hints | cls.__fields
-        cls.__defaults = frozenset(attr for attr in cls.__hints
-                                   if hasattr(cls, attr))
+        fields = frozenset(_utils.extract_fields(cls.__template__))
+        hints = types.MappingProxyType(dict(_utils.filter_hints(cls)))
+        defaults = frozenset(attr for attr in hints if hasattr(cls, attr))
+        cls.__cls_store = _utils.Store(fields=fields,
+                                       hints=hints,
+                                       registered=fields.union(hints),
+                                       defaults=defaults)
 
     def __init__(self, **kwargs: t.Any) -> None:
-        self.__kwargs = kwargs
-        self._validate_kwargs(self.__kwargs)
-        self.__set_attrs(self.__kwargs)
-        self.__msg = self.__template__.format(**self.as_dict())
-        super().__init__(self.__msg)
+        self.__kwargs = kwargs.copy()
+        self.__process_features()
+        self.__populate_attrs()
+        data = self.as_dict()
+        message = self._process_user(store=self.__cls_store,
+                                     kwargs=kwargs,
+                                     message=self.__process_template(data))
+        super().__init__(message)
 
-    def _validate_kwargs(self, kwargs: dict[str, t.Any]) -> None:
-        if not self.__features__:
-            return
+    def __process_features(self):
+        store = self.__cls_store
+        kws = frozenset(self.__kwargs)
 
-        kws = frozenset(kwargs)
         if Features.FORBID_MISSING_FIELDS in self.__features__:
-            if missing := (self.__registered - self.__defaults - kws):
-                raise TypeError(f"Missing arguments: {_utils.join(missing)}")
+            if missing := (store.registered - store.defaults - kws):
+                msg = f"Missing arguments: {_utils.join(missing)}"
+                raise TypeError(msg)  # ? exc type
 
         if Features.FORBID_UNDECLARED_FIELDS in self.__features__:
-            if undeclared := (kws - self.__registered):
+            if undeclared := (kws - store.registered):
                 msg = f"Undeclared arguments: {_utils.join(undeclared)}"
-                raise TypeError(msg)
+                raise TypeError(msg)  # ? exc type
 
-    def __set_attrs(self, kwargs: dict[str, t.Any]) -> None:
-        for k, v in kwargs.items():
-            if k in self.__hints:
+        if Features.FORBID_WRONG_TYPES in self.__features__:
+            errors = []
+            for k, v in self.__kwargs.items():
+                type_ = store.hints[k]
+                if not isinstance(v, type_):
+                    errors.append((k, type_, type(v)))
+            if errors:
+                tpl = "field '{}' must be '{}' but is '{}'"
+                chunks = (tpl.format(*err) for err in errors)
+                msg = "Unexpected types: " + _utils.join(chunks, ";")
+                raise TypeError(msg)  # ? exc type
+
+    def __populate_attrs(self):
+        for k, v in self.__kwargs.items():
+            if k in self.__cls_store.hints:
                 setattr(self, k, v)
 
-    def __repr__(self) -> str:
-        kwargs = _utils.join_kwargs(**self.get_kwargs(True))
-        return f"{type(self).__name__}({kwargs})"
+    def __process_template(self, data) -> str:
+        try:
+            return self.__template__.format(**data)
+        except Exception as e:  # ?
+            msg = ("Failed to format template with provided kwargs: "
+                   + _utils.join_kwargs(**self.__kwargs))
+            raise ValueError(msg) from e
 
-    def __str__(self) -> str:
-        return f"{type(self).__name__}: {self.__msg}"
+    def _process_user(self,
+                      store: _utils.Store,
+                      kwargs: dict[str, t.Any],
+                      message: str) -> str:
+        return message
+
+    def __repr__(self) -> str:
+        kwargs = _utils.join_kwargs(**self.as_dict())
+        return f"{self.__class__.__name__}({kwargs})"
 
     def __copy__(self):
-        return type(self)(**self.get_kwargs(True))
+        return type(self)(**self.as_dict())
 
     def __deepcopy__(self, memo):
         if self not in memo:
-            memo[self] = type(self)(**{k: copy.deepcopy(v)
-                                       for k, v in self.get_kwargs(True)})
+            kwargs = {k: copy.deepcopy(v) for k, v in self.as_dict()}
+            memo[self] = type(self)(**kwargs)
         return memo[self]
 
     def __reduce__(self):
@@ -77,18 +106,17 @@ class Error(Exception):
         parent[1] = tuple()
         return tuple(parent)
 
-    def get_message(self) -> str:
-        return self.__msg
+    def as_str(self) -> str:
+        return f"{self.__class__.__name__}: {self}"
 
-    def get_kwargs(self, enriched: bool = False) -> dict[str, t.Any]:
-        kwargs = self.__kwargs.copy()
-        if enriched:
-            for field in self.__defaults:
-                kwargs.setdefault(field, getattr(self, field))
-        return kwargs
+    def as_kwargs(self) -> dict[str, t.Any]:
+        return self.__kwargs.copy()
 
     def as_dict(self) -> dict[str, t.Any]:
-        return self.get_kwargs(True)
+        d = self.__kwargs.copy()
+        for field in self.__cls_store.defaults:
+            d.setdefault(field, getattr(self, field))
+        return d
 
 
 def factory(func, self: bool = False) -> functools.cached_property:
