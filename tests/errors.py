8b85e3c4b271d04a78ed5e892a865626a4a76162
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-12-28 00:04:18 +0300

    Cover as_str with units

--- tests/errors.py
+++ tests/errors.py
@@ -4,6 +4,9 @@ from izulu import root
 
 
 class Exc(root.Error):
+
+    __template__ = "The {name} is {age} years old"
+
     MY_CONST: t.ClassVar[int] = 42
 
     name: str
--- tests/test_root.py
+++ tests/test_root.py
@@ -4,6 +4,18 @@ import uuid
 import pytest
 
 from izulu import root
+from tests import errors
+
+
+@pytest.mark.parametrize(
+    "err,expected",
+    (
+            (root.Error(), "Error: Unspecified error"),
+            (errors.Exc(name="John", age=10), "Exc: The John is 10 years old"),
+    )
+)
+def test_as_str(err, expected):
+    assert err.as_str() == expected
 
 
 @pytest.mark.parametrize("flag", (True, False))
