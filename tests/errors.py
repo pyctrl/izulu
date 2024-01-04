82c625343666b8eca8a8c094cec263cf3a596f68
Dima Burmistrov <pyctrl.dev@gmail.com>
2024-01-04 23:15:50 +0300

    Add unit tests for store

--- tests/errors.py
+++ tests/errors.py
@@ -1,6 +1,73 @@
+import datetime
 import typing as t
 
 from izulu import root
+
+
+RootError = root.Error
+
+
+class TemplateOnlyError(root.Error):
+
+    __template__ = "The {name} is {age} years old"
+
+
+class AttributesOnlyError(root.Error):
+
+    __template__ = "Static message template"
+
+    name: str
+    age: int
+
+
+class AttributesWithStaticDefaultsError(root.Error):
+
+    __template__ = "Static message template"
+
+    name: str
+    age: int = 0
+
+
+class AttributesWithDynamicDefaultsError(root.Error):
+
+    __template__ = "Static message template"
+
+    name: str
+    age: int = root.factory(int)
+
+
+class ClassVarsError(root.Error):
+
+    __template__ = "Static message template"
+
+    name: t.ClassVar[str] = "Username"
+    age: t.ClassVar[int] = 42
+
+
+class MixedError(root.Error):
+
+    __template__ = "The {name} is {age} years old with {note}"
+
+    entity: t.ClassVar[str] = "The Entity"
+
+    name: str
+    age: int = 0
+    timestamp: datetime.datetime = root.factory(datetime.datetime.now)
+    my_type: str = root.factory(lambda self: self.__class__.__name__,
+                                self=True)
+
+
+class DerivedError(MixedError):
+
+    __template__ = "The {name} {surname} is {age} years old with {note}"
+
+    entity: t.ClassVar[str] = "The Entity"
+
+    surname: str
+    location: tuple[float, float] = (50.3, 3.608)
+    updated_at: datetime.datetime = root.factory(datetime.datetime.now)
+    full_name: str = root.factory(lambda self: f"{self.name} {self.surname}",
+                                  self=True)
 
 
 class Exc(root.Error):
--- tests/test_root.py
+++ tests/test_root.py
@@ -1,3 +1,5 @@
+import datetime
+import types
 from unittest import mock
 import uuid
 
@@ -8,7 +10,104 @@ from tests import errors
 
 
 @pytest.mark.parametrize(
-    "err,expected",
+    ("kls", "fields", "hints", "registered", "defaults"),
+    (
+            (
+                    root.Error,
+                    frozenset(),
+                    types.MappingProxyType({}),
+                    frozenset(),
+                    frozenset(),
+            ),
+            (
+                    errors.TemplateOnlyError,
+                    frozenset(("name", "age")),
+                    types.MappingProxyType({}),
+                    frozenset(("name", "age")),
+                    frozenset(),
+            ),
+            (
+                    errors.AttributesOnlyError,
+                    frozenset(),
+                    types.MappingProxyType(dict(name=str, age=int)),
+                    frozenset(("name", "age")),
+                    frozenset(),
+            ),
+            (
+                    errors.AttributesWithStaticDefaultsError,
+                    frozenset(),
+                    types.MappingProxyType(dict(name=str, age=int)),
+                    frozenset(("name", "age")),
+                    frozenset(("age",)),
+            ),
+            (
+                    errors.AttributesWithDynamicDefaultsError,
+                    frozenset(),
+                    types.MappingProxyType(dict(name=str, age=int)),
+                    frozenset(("name", "age")),
+                    frozenset(("age",)),
+            ),
+            (
+                    errors.ClassVarsError,
+                    frozenset(),
+                    types.MappingProxyType({}),
+                    frozenset(),
+                    frozenset(),
+            ),
+            (
+                    errors.MixedError,
+                    frozenset(("name", "age", "note")),
+                    types.MappingProxyType(dict(name=str,
+                                                age=int,
+                                                timestamp=datetime.datetime,
+                                                my_type=str)),
+                    frozenset(("name", "age", "note", "timestamp", "my_type")),
+                    frozenset(("age", "timestamp", "my_type")),
+            ),
+            (
+                    errors.DerivedError,
+                    frozenset(("name", "surname", "age", "note")),
+                    types.MappingProxyType(dict(name=str,
+                                                age=int,
+                                                timestamp=datetime.datetime,
+                                                my_type=str,
+                                                surname=str,
+                                                location=tuple[float, float],
+                                                updated_at=datetime.datetime,
+                                                full_name=str)),
+                    frozenset(("name",
+                               "age",
+                               "note",
+                               "timestamp",
+                               "my_type",
+                               "surname",
+                               "location",
+                               "updated_at",
+                               "full_name")),
+                    frozenset(("age",
+                               "timestamp",
+                               "my_type",
+                               "location",
+                               "updated_at",
+                               "full_name")),
+            ),
+    )
+)
+def test_class_stores(kls, fields, hints, registered, defaults):
+    store = getattr(kls, "_Error__cls_store")
+
+    assert type(store.fields) is type(fields)
+    assert store.fields == fields
+    assert type(store.hints) is type(hints)
+    assert store.hints == hints
+    assert type(store.registered) is type(registered)
+    assert store.registered == registered
+    assert type(store.defaults) is type(defaults)
+    assert store.defaults == defaults
+
+
+@pytest.mark.parametrize(
+    ("err", "expected"),
     (
             (root.Error(), "izulu.root.Error()"),
             (errors.Exc(name="John", age=10),
@@ -20,7 +119,7 @@ def test_repr(err, expected):
 
 
 @pytest.mark.parametrize(
-    "err,expected",
+    ("err", "expected"),
     (
             (root.Error(), "Error: Unspecified error"),
             (errors.Exc(name="John", age=10), "Exc: The John is 10 years old"),
@@ -31,7 +130,7 @@ def test_as_str(err, expected):
 
 
 @pytest.mark.parametrize(
-    "err,expected",
+    ("err", "expected"),
     (
             (root.Error(), dict()),
             (errors.Exc(name="John", age=10), dict(name="John", age=10)),
@@ -42,7 +141,7 @@ def test_as_kwargs(err, expected):
 
 
 @pytest.mark.parametrize(
-    "err,expected",
+    ("err", "expected"),
     (
             (root.Error(), dict()),
             (errors.Exc(name="John", age=10), dict(name="John", age=10)),
