ec95a933831425b8d72a7053679da556bbb9f5f1
Dima Burmistrov <pyctrl.dev@gmail.com>
2025-03-08 20:40:50 +0400

    Reraise action compilations

--- izulu/_reraise.py
+++ izulu/_reraise.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+
 import contextlib
 import functools
 import typing as t
+
+_T_FACTORY = t.Callable[
+    [t.Type[Exception], Exception, dict],
+    t.Optional[Exception],
+]
+_T_ACTION = t.Union[None, str, t.Type[Exception], _T_FACTORY]
+
+_MISSING = object()
 
 
 class FatalMixin:
@@ -9,12 +19,72 @@ class FatalMixin:
 
 class ReraisingMixin:
 
-    __reraising__: bool = False
-    __reraising = []  # type: ignore[var-annotated] # TODO(d.burmistrov): rules
+    __reraising: t.Tuple[t.Tuple[t.Type[Exception], t.Callable], ...]
+
+    __reraising__: t.Union[
+        bool,
+        t.Tuple[t.Tuple[t.Type[Exception], _T_ACTION], ...],
+    ] = False
 
     @classmethod
     def __get_reraising(cls):
         return cls.__dict__.get("__reraising__", False)
+
+    @classmethod
+    def _compile_action(
+            cls,
+            action: _T_ACTION,
+    ) -> t.Callable[[Exception, dict], t.Optional[Exception]]:
+        if action is None:
+
+            def compiled_action(
+                orig: Exception,
+                kwargs: dict[str, t.Any],
+            ) -> t.Optional[Exception]:
+                return None
+
+        elif (
+            action is getattr(t, "Self", _MISSING)
+            or action == cls.__qualname__
+        ):
+
+            def compiled_action(
+                orig: Exception,
+                kwargs: dict[str, t.Any],
+            ) -> t.Optional[Exception]:
+                kls = t.cast(t.Type[Exception], cls)
+                return kls(**kwargs)
+
+        elif isinstance(action, str):
+
+            def compiled_action(
+                orig: Exception,
+                kwargs: dict[str, t.Any],
+            ) -> t.Optional[Exception]:
+                action_ = getattr(cls, t.cast(str, action))
+                return action_(orig, kwargs)
+
+        elif isinstance(action, type) and issubclass(action, Exception):
+
+            def compiled_action(
+                orig: Exception,
+                kwargs: dict[str, t.Any],
+            ) -> t.Optional[Exception]:
+                return t.cast(t.Type[Exception], action)(**kwargs)
+
+        elif callable(action):
+
+            def compiled_action(
+                orig: Exception,
+                kwargs: dict[str, t.Any],
+            ) -> t.Optional[Exception]:
+                kls = t.cast(t.Type[Exception], cls)
+                return t.cast(_T_FACTORY, action)(kls, orig, kwargs)
+
+        else:
+            raise ValueError(f"Unsupported action: {action}")
+
+        return compiled_action
 
     @classmethod
     def remap(
