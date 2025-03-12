485a6ddeaba768983f8bed854be1832a92411270
Dima Burmistrov <pyctrl.dev@gmail.com>
2025-03-12 17:17:53 +0400

    Add initial tests for reraise

---
+++ tests/reraise/test_fatal.py
@@ -0,0 +1,14 @@
+from izulu import _reraise
+
+import pytest
+
+
+def test_fatal_direct_inheritance():
+    type("Klass", (_reraise.FatalMixin,), {})
+
+
+def test_fatal_indirect_inheritance():
+    kls = type("Klass", (_reraise.FatalMixin,), {})
+
+    with pytest.raises(TypeError):
+        type("Klass2", (kls,), {})
