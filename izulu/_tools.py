2481c968dc651c1027222e683cd214fe3f5a80e4
Dima Burmistrov <pyctrl.dev@gmail.com>
2025-07-17 14:54:39 +0400

    Improve suppress tool

--- izulu/_tools.py
+++ izulu/_tools.py
@@ -6,9 +6,9 @@ _LOG = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
-def suppress(*excs: Exception) -> t.Generator[None, None, None]:
-    excs = excs or Exception
+def suppress(*excs: t.Type[Exception]) -> t.Generator[None, None, None]:
+    exc_targets = excs or Exception
     try:
         yield
-    except excs as e:
+    except exc_targets as e:
         _LOG.error("Error suppressed: %s", e)
