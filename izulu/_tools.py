260b6540e1ccb8c0167b4a6bdcec253b83e652f6
Dima Burmistrov <pyctrl.dev@gmail.com>
2025-07-30 09:54:27 +0400

    Add exclude option for suppress tool

--- izulu/_tools.py
+++ izulu/_tools.py
@@ -6,9 +6,14 @@ _LOG = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
-def suppress(*excs: t.Type[Exception]) -> t.Generator[None, None, None]:
+def suppress(
+    *excs: t.Type[Exception],
+    exclude: t.Optional[t.Type[Exception]] = None,
+) -> t.Generator[None, None, None]:
     exc_targets = excs or Exception
     try:
         yield
     except exc_targets as e:
+        if exclude and isinstance(e, exclude):
+            raise
         _LOG.error("Error suppressed: %s", e)
